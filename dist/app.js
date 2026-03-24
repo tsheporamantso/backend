d2cafd18232bb44835f34bd8ba402d1266897631
tsheporamantso <tshepo.ramantso@outlook.com>
2026-03-24 13:10:49 +0200

    Add start function to connect to DB and start server

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("hello data");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const env_1 = require("./utils/env");
+const connect_1 = __importDefault(require("./db/connect"));
+require("dotenv").config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+async function start() {
+    try {
+        await (0, connect_1.default)((0, env_1.getEnvVariable)("MONGO_URI"));
+        console.log("CONNECTED TO DB...");
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+start();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("hello data");
+import express, { Request, Response } from "express";
+import { getEnvVariable } from "./utils/env";
+import connectDB from "./db/connect";
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+async function start() {
+  try {
+    await connectDB(getEnvVariable("MONGO_URI"));
+    console.log("CONNECTED TO DB...");
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
