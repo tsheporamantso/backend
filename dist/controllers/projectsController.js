fb35d9b4c8c8af473ba19404f16c666d03f01b8e
tsheporamantso <tshepo.ramantso@outlook.com>
2026-03-25 10:36:22 +0200

    Add update project controller

--- dist/controllers/projectsController.js
+++ dist/controllers/projectsController.js
@@ -4,11 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Project_1 = __importDefault(require("../models/Project"));
-const getAllProjects = (req, res) => {
-    res.status(200).json({
-        success: true,
-        data: "Get all projects",
-    });
+const getAllProjects = async (req, res) => {
+    try {
+        const project = await Project_1.default.find({});
+        res.status(200).json({
+            nbHits: project.length,
+            success: true,
+            data: project,
+        });
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({
+                success: false,
+                msg: error.message,
+            });
+        }
+        else {
+            res.status(500).json({
+                success: false,
+                msg: "Something wet wrong",
+            });
+        }
+    }
 };
 const getSingleProject = (req, res) => {
     res.status(200).json({
@@ -39,11 +57,38 @@ const createProject = async (req, res) => {
         }
     }
 };
-const updateProject = (req, res) => {
-    res.status(200).json({
-        success: true,
-        data: "Update project",
-    });
+const updateProject = async (req, res) => {
+    try {
+        const { id: projectID } = req.params;
+        const project = await Project_1.default.findOneAndUpdate({ _id: projectID }, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!project) {
+            res.status(404).json({
+                success: false,
+                msg: `No project with id: ${projectID}`,
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: project,
+        });
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({
+                success: false,
+                msg: error.message,
+            });
+        }
+        else {
+            res.status(500).json({
+                success: false,
+                msg: "Something went wrong",
+            });
+        }
+    }
 };
 const deleteProject = (req, res) => {
     res.status(200).json({
--- src/controllers/projectsController.ts
+++ src/controllers/projectsController.ts
@@ -1,11 +1,27 @@
 import { Request, Response } from "express";
 import Project from "../models/Project";
 
-const getAllProjects = (req: Request, res: Response) => {
-  res.status(200).json({
-    success: true,
-    data: "Get all projects",
-  });
+const getAllProjects = async (req: Request, res: Response) => {
+  try {
+    const project = await Project.find({});
+    res.status(200).json({
+      nbHits: project.length,
+      success: true,
+      data: project,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        success: false,
+        msg: error.message,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        msg: "Something wet wrong",
+      });
+    }
+  }
 };
 
 const getSingleProject = (req: Request, res: Response) => {
@@ -37,11 +53,40 @@ const createProject = async (req: Request, res: Response) => {
   }
 };
 
-const updateProject = (req: Request, res: Response) => {
-  res.status(200).json({
-    success: true,
-    data: "Update project",
-  });
+const updateProject = async (req: Request, res: Response) => {
+  try {
+    const { id: projectID } = req.params;
+    const project = await Project.findOneAndUpdate(
+      { _id: projectID },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    if (!project) {
+      res.status(404).json({
+        success: false,
+        msg: `No project with id: ${projectID}`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: project,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        success: false,
+        msg: error.message,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        msg: "Something went wrong",
+      });
+    }
+  }
 };
 
 const deleteProject = (req: Request, res: Response) => {
