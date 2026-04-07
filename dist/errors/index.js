1528f9c36c47b72ddcd902f7cc6ff9e0faa6bc87
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-07 17:58:25 +0200

    Add Bad request and Unauthenticated custom error functions

---
+++ dist/errors/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const CustomAPIError = require("./custom-error");
+const BadRequest = require("./bad-request");
+const UnauthenticatedError = require("./unauthenticated");
+module.exports = {
+    CustomAPIError,
+    BadRequest,
+    UnauthenticatedError,
+};
