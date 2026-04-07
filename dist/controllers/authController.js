b63e3b2a2d3b749978bea428c864d430e7d7d2b7
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-07 13:22:55 +0200

    Sign and verify user

--- dist/controllers/authController.js
+++ dist/controllers/authController.js
@@ -4,23 +4,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dashboard = exports.login = void 0;
-const async_1 = require("../middleware/async");
+require("dotenv").config();
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const http_status_codes_1 = require("http-status-codes");
-const Auth_1 = __importDefault(require("../models/Auth"));
+const async_1 = require("../middleware/async");
 const custom_error_1 = require("../errors/custom-error");
+const env_1 = require("../utils/env");
 exports.login = (0, async_1.asyncWrapper)(async (req, res) => {
-    const auth = await Auth_1.default.create(req.body);
-    if (!auth) {
+    const { username, password } = req.body;
+    if (!username || !password) {
         throw new custom_error_1.CustomAPIError("Please provide username and password", http_status_codes_1.StatusCodes.BAD_REQUEST);
     }
+    const id = new Date().getDate();
+    const options = {
+        expiresIn: (0, env_1.getEnvVariable)("JWT_EXPIRES_IN"),
+    };
+    const token = jsonwebtoken_1.default.sign({ id, username }, (0, env_1.getEnvVariable)("JWT_SECRET"), options);
     res.status(http_status_codes_1.StatusCodes.OK).json({
         success: true,
-        msg: "Login successful",
+        msg: "User created",
+        token,
     });
 });
 exports.dashboard = (0, async_1.asyncWrapper)(async (req, res) => {
-    res.status(http_status_codes_1.StatusCodes.OK).json({
-        success: true,
-        msg: "Dashboard",
-    });
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new custom_error_1.CustomAPIError("Invalid credentials to access this route", http_status_codes_1.StatusCodes.UNAUTHORIZED);
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = jsonwebtoken_1.default.verify(token, (0, env_1.getEnvVariable)("JWT_SECRET"));
+        res.status(http_status_codes_1.StatusCodes.OK).json({
+            success: true,
+            msg: `Welcome ${decoded.username}`,
+        });
+    }
+    catch (error) {
+        throw new custom_error_1.CustomAPIError("Not authorized to access this route", http_status_codes_1.StatusCodes.UNAUTHORIZED);
+    }
 });
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,28 +1,69 @@
-import { asyncWrapper } from "../middleware/async";
-import { Request, Response, NextFunction } from "express";
+require("dotenv").config();
+import jwt, { SignOptions } from "jsonwebtoken";
+import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import authentication from "../models/Auth";
+import { asyncWrapper } from "../middleware/async";
 import { CustomAPIError } from "../errors/custom-error";
+import { getEnvVariable } from "../utils/env";
 
 export const login = asyncWrapper(async (req: Request, res: Response) => {
-  const auth = await authentication.create(req.body);
+  const { username, password } = req.body;
 
-  if (!auth) {
+  if (!username || !password) {
     throw new CustomAPIError(
       "Please provide username and password",
       StatusCodes.BAD_REQUEST,
     );
   }
 
+  const id = new Date().getDate();
+
+  const options: SignOptions = {
+    expiresIn: getEnvVariable("JWT_EXPIRES_IN") as SignOptions["expiresIn"],
+  };
+
+  const token = jwt.sign(
+    { id, username },
+    getEnvVariable("JWT_SECRET"),
+    options,
+  );
+
   res.status(StatusCodes.OK).json({
     success: true,
-    msg: "Login successful",
+    msg: "User created",
+    token,
   });
 });
 
 export const dashboard = asyncWrapper(async (req: Request, res: Response) => {
-  res.status(StatusCodes.OK).json({
-    success: true,
-    msg: "Dashboard",
-  });
+  interface JWTPayloadTypes {
+    id: number;
+    username: string;
+  }
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError(
+      "Invalid credentials to access this route",
+      StatusCodes.UNAUTHORIZED,
+    );
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      getEnvVariable("JWT_SECRET"),
+    ) as JWTPayloadTypes;
+    res.status(StatusCodes.OK).json({
+      success: true,
+      msg: `Welcome ${decoded.username}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError(
+      "Not authorized to access this route",
+      StatusCodes.UNAUTHORIZED,
+    );
+  }
 });
