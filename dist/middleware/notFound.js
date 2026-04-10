8e7c0d3fc0949a5916bae44915d27713e0b76336
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-10 07:24:18 +0200

    chore: remove dist/ from version control

--- dist/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = notFound;
-const http_status_codes_1 = require("http-status-codes");
-function notFound(req, res) {
-    res.status(http_status_codes_1.StatusCodes.NOT_FOUND).json({
-        msg: "Route not found",
-    });
-}
