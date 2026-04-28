05bb743c86eba7ade14021169bc03f2b92d9fc37
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-28 16:26:26 +0200

    chore: export AuthUser type

--- src/utils/getAuthUser.ts
+++ src/utils/getAuthUser.ts
@@ -1,7 +1,7 @@
 import { Request } from "express";
 import CustomError from "../errors";
 
-type AuthUser = NonNullable<Request["user"]>;
+export type AuthUser = NonNullable<Request["user"]>;
 
 export const getAuthUser = (req: Request): AuthUser => {
   if (!req.user) {
