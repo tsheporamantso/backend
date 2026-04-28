7a73c68e1b26d63549df7565e9a388101097258d
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-28 10:42:40 +0200

    feat(errors): add unauthorized custom error class

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,9 +1,13 @@
 import { CustomAPIError } from "./custom-error";
-import { BadRequest } from "./bad-request";
+import { BadRequestError } from "./bad-request";
 import { UnauthenticatedError } from "./unauthenticated";
+import { NotFoundError } from "./not-found-error";
+import { UnauthorizedError } from "./unauthorized-error";
 
-module.exports = {
+export default {
   CustomAPIError,
-  BadRequest,
+  BadRequestError,
   UnauthenticatedError,
+  NotFoundError,
+  UnauthorizedError,
 };
