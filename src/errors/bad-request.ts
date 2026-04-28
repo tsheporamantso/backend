89da93eba8c906c325b7078a086e0595f8e75a9f
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-28 10:15:52 +0200

    chore(errors): gave appropriate custom error class name

--- src/errors/bad-request.ts
+++ src/errors/bad-request.ts
@@ -1,7 +1,7 @@
 import { CustomAPIError } from "./custom-error";
 import { StatusCodes } from "http-status-codes";
 
-export class BadRequest extends CustomAPIError {
+export class BadRequestError extends CustomAPIError {
   constructor(message: string, statusCode: number = StatusCodes.BAD_REQUEST) {
     super(message, statusCode);
   }
