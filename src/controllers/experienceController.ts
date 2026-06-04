b32eb4fd99a9f76193657bb449187a7b22bc4195
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-04 13:51:29 +0200

    refactor(controllers): add crud functionality

--- src/controllers/experienceController.ts
+++ src/controllers/experienceController.ts
@@ -1,22 +1,70 @@
 import { asyncWrapper } from "../middleware/async";
 import { StatusCodes } from "http-status-codes";
+import Experience from "../models/Experience";
+import { createCustomError } from "../errors/custom-error";
 
 export const getAllExperiences = asyncWrapper(async (req, res) => {
-  res.status(StatusCodes.OK).json("get all experiences");
+  const experiences = await Experience.find({});
+  res
+    .status(StatusCodes.OK)
+    .json({ nbHits: experiences.length, success: true, experiences });
 });
 
-export const getSingleExperience = asyncWrapper(async (req, res) => {
-  res.status(StatusCodes.OK).json("get single experiences");
+export const getSingleExperience = asyncWrapper(async (req, res, next) => {
+  const { id: experienceId } = req.params;
+  const experience = await Experience.findOne({ _id: experienceId });
+
+  if (!experience) {
+    return next(
+      createCustomError(
+        `No experience with id: ${experienceId}`,
+        StatusCodes.NOT_FOUND,
+      ),
+    );
+  }
+  res.status(StatusCodes.OK).json({ success: true, experience });
 });
 
 export const createExperience = asyncWrapper(async (req, res) => {
-  res.status(StatusCodes.CREATED).json("create experience");
+  const experience = await Experience.create(req.body);
+  res.status(StatusCodes.CREATED).json({ success: true, experience });
 });
 
-export const updateExperience = asyncWrapper(async (req, res) => {
-  res.status(StatusCodes.CREATED).json("update experience");
+export const updateExperience = asyncWrapper(async (req, res, next) => {
+  const { id: experienceId } = req.params;
+  const experience = await Experience.findOneAndUpdate(
+    { _id: experienceId },
+    req.body,
+    {
+      returnDocument: "after",
+      runValidators: true,
+    },
+  );
+  if (!experience) {
+    return next(
+      createCustomError(
+        `No experience with id: ${experienceId}`,
+        StatusCodes.NOT_FOUND,
+      ),
+    );
+  }
+  res.status(StatusCodes.OK).json({ success: true, experience });
 });
 
-export const deleteExperience = asyncWrapper(async (req, res) => {
-  res.status(StatusCodes.CREATED).json("delete experience");
+export const deleteExperience = asyncWrapper(async (req, res, next) => {
+  const { id: experienceId } = req.params;
+  const experience = await Experience.deleteOne({ _id: experienceId });
+
+  if (!experience) {
+    return next(
+      createCustomError(
+        `No experience with id: ${experienceId}`,
+        StatusCodes.NOT_FOUND,
+      ),
+    );
+  }
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ success: true, msg: "experience deleted successfully" });
 });
